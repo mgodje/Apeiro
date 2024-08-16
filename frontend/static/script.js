9baae65804e08f60ca2aa54248698d86abe41f4c
VMFenix8 <lalavezc@ucsc.edu>
2024-08-16 13:48:13 -0700

    Fix Features

--- frontend/static/script.js
+++ frontend/static/script.js
@@ -37,7 +37,7 @@ const scene = new THREE.Scene()
 
 // textures
 const textureLoader = new THREE.TextureLoader()
-const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+const gradientTexture = textureLoader.load('./textures/gradients/3.jpg')
 gradientTexture.magFilter = THREE.NearestFilter
 
 // material
